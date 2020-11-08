229c107b089bb5b5967bbc7e3d44411a119b91a4
Piromancer <Den2000_09@mail.ru>
2020-11-09 00:55:34 +0300

    Added some mocks

--- src/lab2/MockExamples.java
+++ src/lab2/MockExamples.java
@@ -31,26 +31,32 @@ public class MockExamples {
     }
 
     public static BigDecimal ctg(double x){
+        if (x == -Math.PI/3) return BigDecimal.valueOf(-0.577);
+        if (x == -Math.PI/4) return BigDecimal.valueOf(-1);
+        if (x == -Math.PI/6) return BigDecimal.valueOf(-1.732);
+        if (x == -Math.PI/2) return BigDecimal.valueOf(0);
         return null;
     }
 
     public static BigDecimal csc(double x){
+        if (x == -Math.PI/3) return BigDecimal.valueOf(-1.154);
+        if (x == -Math.PI/4) return BigDecimal.valueOf(-1.4);
+        if (x == -Math.PI/6) return BigDecimal.valueOf(-2);
+        if (x == 0) return BigDecimal.valueOf(1);
         return null;
     }
 
     public static BigDecimal ln(double x){
-        if (x == -Math.PI/3) return BigDecimal.valueOf(-1.732);
-        if (x == -Math.PI/4) return BigDecimal.valueOf(-1);
-        if (x == -Math.PI/6) return BigDecimal.valueOf(-0.577);
-        if (x == 0) return BigDecimal.valueOf(0);
+        if (x == 1) return BigDecimal.valueOf(0);
+        if (x == Math.E) return BigDecimal.valueOf(1);
+        if (x == 4) return BigDecimal.valueOf(1.386);
         return null;
     }
 
     public static BigDecimal log(double x, int base){
-        if (x == -Math.PI/3) return BigDecimal.valueOf(-1.732);
-        if (x == -Math.PI/4) return BigDecimal.valueOf(-1);
-        if (x == -Math.PI/6) return BigDecimal.valueOf(-0.577);
-        if (x == 0) return BigDecimal.valueOf(0);
+        if (x == 1) return BigDecimal.valueOf(0);
+        if (x == 4 && base == 2) return BigDecimal.valueOf(2);
+        if (x == 27 && base == 3) return BigDecimal.valueOf(3);
         return null;
     }
 
